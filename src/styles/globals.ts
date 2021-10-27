2d02c832de1a64a5dc17fd78f2bc8e3d6fa47ed5
Murilo Messias <murilo.messias@gmail.com>
2021-10-27 17:14:23 -0300

    Add Heebo font files

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,22 +1,42 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Heebo';
+    font-style: normal;
+    font-display: swap;
+    font-weight: 400;
+    src: local('Hebbo Regular'),
+          url('/fonts/heebo-v12-latin-regular.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Heebo';
+    font-style: normal;
+    font-display: swap;
+    font-weight: 500;
+    src: local('Hebbo Medium'),
+          url('/fonts/heebo-v12-latin-500.woff2') format('woff2');
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
 
   html {
     font-size: 62.5%;
   }
 
-  html, body, #__next {
-    height: 100%;
-  }
-
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'Heebo', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `
 export default GlobalStyles
