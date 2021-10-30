321413ab31f89333c3861050f2230426e9160b03
Murilo Messias <murilo.messias@gmail.com>
2021-10-30 16:16:32 -0300

    Add Modal component

---
+++ src/components/modal/modal-stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import { Modal } from '.'
+
+export default {
+  title: 'Modal',
+  component: Modal,
+} as Meta
+
+export const Default: Story = () => <Modal />
