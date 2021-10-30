bd8d3fc81334606fee85161bdb61308083da467d
Murilo Messias <murilo.messias@gmail.com>
2021-10-30 16:38:07 -0300

    Add handleSign structure

--- src/templates/login/login.tsx
+++ src/templates/login/login.tsx
@@ -1,29 +1,45 @@
 import { Input, Logo, Tooltip } from 'components'
+import { useState } from 'react'
 import * as S from './login-styles'
 
-export const Login = () => (
-  <S.Wrapper>
-    <S.Container>
-      <S.LogoContainer>
-        <Logo />
-      </S.LogoContainer>
+export const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const error = 'Email e/ou senha incorreta'
 
-      <S.Form>
-        <Input
-          type='email'
-          name='email'
-          label='E-mail'
-          placeholder='books@ioasys.com.br'
-        />
-        <Input
-          type='password'
-          name='password'
-          label='Senha'
-          placeholder='*******'
-          button='Entrar'
-        />
-        <Tooltip error='Email e/ou senha incorreta' active />
-      </S.Form>
-    </S.Container>
-  </S.Wrapper>
-)
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    const data = { email, password }
+    console.log(data)
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <S.LogoContainer>
+          <Logo />
+        </S.LogoContainer>
+
+        <S.Form onSubmit={handleLogin}>
+          <Input
+            type='email'
+            name='email'
+            label='E-mail'
+            placeholder='books@ioasys.com.br'
+            onInputChange={setEmail}
+          />
+          <Input
+            type='password'
+            name='password'
+            label='Senha'
+            placeholder='*******'
+            button='Entrar'
+            onInputChange={setPassword}
+          />
+          {!!error && <Tooltip error={error} active />}
+        </S.Form>
+      </S.Container>
+    </S.Wrapper>
+  )
+}
